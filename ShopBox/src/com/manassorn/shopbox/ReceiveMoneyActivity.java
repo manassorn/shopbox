d20ffa54e70227a13294b96a7bdefa6c217fc664
Manassorn <manassorn@gmail.com>
2013-08-03 21:25:57 +0700

    click done receive money

--- ShopBox/src/com/manassorn/shopbox/ReceiveMoneyActivity.java
+++ ShopBox/src/com/manassorn/shopbox/ReceiveMoneyActivity.java
@@ -7,13 +7,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 import com.manassorn.shopbox.db.BillItemDao;
@@ -22,7 +25,8 @@ import com.manassorn.shopbox.db.SellBillDao;
 import com.manassorn.shopbox.value.BillItem;
 import com.manassorn.shopbox.value.SellBill;
 
-public class ReceiveMoneyActivity extends Activity implements OnClickListener {
+public class ReceiveMoneyActivity extends Activity implements OnClickListener,
+		OnEditorActionListener {
 	private static final String TAG = ReceiveMoneyActivity.class.getSimpleName();
 	private ArrayList<BillItem> billItems;
 	private int billId;
@@ -40,13 +44,16 @@ public class ReceiveMoneyActivity extends Activity implements OnClickListener {
 		ok.setOnClickListener(this);
 
 		Bundle bundle = getIntent().getExtras();
-		if(bundle != null) {
+		if (bundle != null) {
 			billItems = bundle.getParcelableArrayList("BILL_ITEM_ARRAY");
 			total = bundle.getDouble("TOTAL");
 		}
 
 		TextView totalView = (TextView) findViewById(R.id.total);
 		totalView.setText(String.format("ß%,.2f", total));
+
+		EditText receiveMoneyText = (EditText) findViewById(R.id.receive_money);
+		receiveMoneyText.setOnEditorActionListener(this);
 	}
 
 	@Override
@@ -58,7 +65,7 @@ public class ReceiveMoneyActivity extends Activity implements OnClickListener {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId() == android.R.id.home) {
+		if (item.getItemId() == android.R.id.home) {
 			onBackPressed();
 			return true;
 		}
@@ -69,11 +76,18 @@ public class ReceiveMoneyActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.ok_button:
-				if (validateInput()) {
-					billId = insertBill();
-					startGiveChangeActivity();
-				}
+				ok();
 		}
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		if ((event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER)
+				|| (actionId == EditorInfo.IME_ACTION_DONE)) {
+			ok();
+			return true;
+		}
+		return false;
 	}
 
 	protected boolean validateInput() {
@@ -94,20 +108,20 @@ public class ReceiveMoneyActivity extends Activity implements OnClickListener {
 	protected int insertBill() {
 		SellBill sellBill = new SellBill(billItems);
 		sellBill.setReceiveMoney(getReceiveMoney());
-//		long billId = billDbAdapter.insertBill(bill);
+		// long billId = billDbAdapter.insertBill(bill);
 		DbHelper dbHelper = DbHelper.getHelper(this);
 		SellBillDao sellBillDao = SellBillDao.getInstance(dbHelper);
 		BillItemDao billItemDao = BillItemDao.getInstance(dbHelper);
 		try {
 			int billId = sellBillDao.insert(sellBill);
-			for(BillItem billItem : billItems) {
+			for (BillItem billItem : billItems) {
 				billItem.setBillId(billId);
 			}
 			billItemDao.insert(billItems);
 		} catch (SQLException e) {
 			Log.e(TAG, "Database Error", e);
 		}
-		
+
 		return (int) billId;
 	}
 
@@ -119,10 +133,17 @@ public class ReceiveMoneyActivity extends Activity implements OnClickListener {
 		intent.putExtra("TOTAL", total);
 		startActivity(intent);
 	}
-	
+
 	protected double getReceiveMoney() {
 		EditText receiveMoneyText = (EditText) findViewById(R.id.receive_money);
 		double receiveMoney = Double.parseDouble(receiveMoneyText.getText().toString());
 		return receiveMoney;
+	}
+
+	protected void ok() {
+		if (validateInput()) {
+			billId = insertBill();
+			startGiveChangeActivity();
+		}
 	}
 }
