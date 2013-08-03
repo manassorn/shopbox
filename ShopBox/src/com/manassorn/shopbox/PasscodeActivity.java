b18e07b2b82bf4fb07ccbe6948b63deedbd74005
Manassorn <manassorn@gmail.com>
2013-08-03 23:40:10 +0700

    passcode activity

--- ShopBox/src/com/manassorn/shopbox/PasscodeActivity.java
+++ ShopBox/src/com/manassorn/shopbox/PasscodeActivity.java
@@ -2,23 +2,31 @@ package com.manassorn.shopbox;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
-public class PasscodeActivity extends Activity implements TextWatcher {
+public class PasscodeActivity extends Activity implements TextWatcher, OnClickListener {
 	EditText[] passcodeDigits = new EditText[4];
 	EditText passcode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_passcode);
+		InterceptBackPressedLayout root = new InterceptBackPressedLayout(this);
+		getLayoutInflater().inflate(R.layout.activity_passcode, root, true);
+		setContentView(root);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		passcodeDigits[0] = (EditText) findViewById(R.id.digit1);
 		passcodeDigits[1] = (EditText) findViewById(R.id.digit2);
@@ -27,12 +35,46 @@ public class PasscodeActivity extends Activity implements TextWatcher {
 
 		passcode = (EditText) findViewById(R.id.passcode);
 		passcode.addTextChangedListener(this);
+		
+		for(EditText passcodeDigit : passcodeDigits) {
+			passcodeDigit.setOnClickListener(this);
+		}
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// sometime show, sometime not
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.showSoftInput(passcode, InputMethodManager.SHOW_IMPLICIT);
+			}
+		}, 200);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == android.R.id.home) {
+			onBackPressed();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	@Override
 	public void onBackPressed() {
 		finish();
 		overridePendingTransition(R.anim.stay_still, R.anim.slide_down_out);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// passcode.requestFocus(); doesn't work
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.showSoftInput(passcode, InputMethodManager.SHOW_IMPLICIT);
 	}
 
 	boolean verifyPasscode(String text) {
@@ -83,4 +125,31 @@ public class PasscodeActivity extends Activity implements TextWatcher {
 	public void onTextChanged(CharSequence s, int start, int before, int count) {
 	}
 
+	public static class InterceptBackPressedLayout extends FrameLayout {
+
+		public InterceptBackPressedLayout(Context context) {
+			super(context);
+			LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+			setLayoutParams(lp);
+		}
+
+		@Override
+		public boolean dispatchKeyEventPreIme(KeyEvent event) {
+			if(event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+				KeyEvent.DispatcherState state = getKeyDispatcherState();
+				if(state != null) {
+					if(event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0) {
+						state.startTracking(event, this);
+						return true;
+					} else if(event.getAction() == KeyEvent.ACTION_UP && !event.isCanceled() && state.isTracking(event)) {
+						((Activity) getContext()).onBackPressed();
+						return true;
+					}
+				}
+			}
+			return super.dispatchKeyEventPreIme(event);
+		}
+		
+		
+	}
 }
